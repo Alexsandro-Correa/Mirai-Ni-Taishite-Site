8f01735a06c275f5190aa9d96c3934b0a117724f
Alexsandro-Correa <alexsandro19031@hotmail.com>
2023-08-07 21:33:05 -0300

    "Primeiro commit"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DatagameComponent } from './datagame/datagame.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatagameComponent
   ],
   imports: [
     BrowserModule,
